833268529ba08782c37c191b2002cf258502e065
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-04-12 19:09:18 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ require_relative '../models/team.rb'
 
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
+    set :views, Proc.new { File.join(root, "./views/") }
 
     get '/' do
       erb :super_hero
